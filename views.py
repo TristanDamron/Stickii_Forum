fff4d0f03bb1a01ea77bf70b6e1ac427bfbeedb1
Tristan Damron <TristanDamron31@gmail.com>
2015-11-21 15:55:31 -0800

    Defined a view for forums

--- views.py
+++ views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import RequestContext, loader
+from .models import *
 
-# Create your views here.
+def forum(request):
+    post = Post.objects.all()
+    template = loader.get_template("forum.html")
+    context = RequestContext(request, {
+        'post': post
+    })
+
+    return HttpResponse(template.render(context))
